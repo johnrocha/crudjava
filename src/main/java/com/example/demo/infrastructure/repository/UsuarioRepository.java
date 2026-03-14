3ca7db142737e42873d492ea3699a2f0806e4278
John Rocha <johnerickrocha@gmail.com>
2026-03-13 23:17:07 -0300

    Aplicação simples de Post, Put, Get e Delete

---
+++ src/main/java/com/example/demo/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.infrastructure.repository;
+
+import com.example.demo.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
